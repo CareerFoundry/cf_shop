75dc014d02a22eaaff9b04624c43634c94b81418
sphillips <phillips.sb@gmail.com>
2015-01-28 15:47:06 +0100

    contact form with text email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,21 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def contact
+	end
+
+	def thank_you
+		# @name = params[:name]
+	 #  @email = params[:email]
+	 #  @message = params[:message]
+  # 	ActionMailer::Base.mail(:from => 'no-reply@example.com', 
+	 #  	:to => @email, 
+	 #  	:subject => "A new contact form message", 
+	 #  	:body => @message).deliver
+  # 	flash[:notice] = "Your message has been delivered."
+  # 	render "static_pages/contact"
+	end
+
   def landing_page
   	@featured_product = Product.first
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
 
   get 'static_pages/contact'
+  post 'static_pages/thank_you'
 
   get 'static_pages/index'
 
