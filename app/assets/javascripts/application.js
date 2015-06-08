4a395d4621d7596eef591ffb35b5834bf627524d
Sean Phillips <phillips.sb@gmail.com>
2015-06-08 16:08:34 +0200

    moving custom JS functions to site.js out of application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(document).on('ready page:load', function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets', 
-		readOnly: true,
-	  score: function() {
-	    return $(this).attr('data-score');
-	  }
-	});
-});
---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,9 @@
+$(document).on('ready page:load', function(){
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets', 
+		readOnly: true,
+	  score: function() {
+	    return $(this).attr('data-score');
+	  }
+	});
+});
