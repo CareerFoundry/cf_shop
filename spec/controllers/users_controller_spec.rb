f232fec0b907eb9ed91442822f641fb248a154db
sam <sam.bogu@gmail.com>
2015-09-16 16:14:52 +0200

    rewritten tests to pass and match the new course content

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,33 +3,36 @@ require 'rails_helper'
 describe UsersController, :type => :controller do
 
   before do
-    # @user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@example.com", password: "abcdefgh", password_confirmation: "abcdefgh")
-    @user = FactoryGirl.create(:user)
+    @user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@example.com", password: "abcdefgh")
+    @user2 = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy2@example.com", password: "abcdefgh")
+    #@user = FactoryGirl.create(:user)
   end
 
-  describe "GET login" do
+  describe "GET #show" do
+     context "User is logged in" do
+       before do
+         sign_in @user
+       end
 
+       it "loads the correct user details" do
+         get :show, id: @user.id
+         expect(response.status).to eq 200
+         expect(assigns(:user)).to eq @user
+       end
+
+       it "doesn't load the second user" do
+         get :show, id: @user2.id
+         expect(response.status).to eq 302
+         expect(response).to redirect_to(root_path)
+       end
+     end
+
+     context "No user is logged in" do
+       it "redirects to login" do
+         get :show, id: @user.id
+         expect(response).to redirect_to(root_path)
+       end
+     end
   end
-
-  # describe "GET show" do
-  #   context "User is logged in" do
-  #     before do
-  #       session[:user_id] = @user.id # simulate a logged in user
-  #     end
-
-  #     it "should load the correct user details" do
-  #       get :show
-  #       expect(response.status).to eq 200
-  #       expect(assigns(:user)).to eq @user
-  #     end
-  #   end
-
-  #   context "No user is logged in" do
-  #     it "should redirect to login" do
-  #       get :show
-  #       expect(response).to redirect_to(log_in_path)
-  #     end
-  #   end
-  # end
 
 end
