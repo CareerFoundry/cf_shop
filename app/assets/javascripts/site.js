96b5923b13d273588ce73c3f4585ab4c5a88ff54
sam <sam.bogu@gmail.com>
2015-09-24 16:17:43 +0200

    added AJAX for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 $(document).on('ready page:load', function(){
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets', 
+	$('.rated').raty({ path: '/assets',
 		readOnly: true,
 	  score: function() {
 	    return $(this).attr('data-score');
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -24,7 +25,7 @@ class CommentsController < ApplicationController
 	end
 
 	private
-    
+
     def comment_params
       params.require(:comment).permit(:user_id, :body, :rating)
     end
