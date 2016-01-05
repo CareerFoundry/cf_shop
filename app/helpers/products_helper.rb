fed6639f60b0e4d0cd1cc5ead9478a2d39a432aa
sam <sam.bogu@gmail.com>
2016-01-05 17:13:43 +0100

    added caching examples

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,7 @@
 module ProductsHelper
+  def cache_key_for_products
+    count          = Product.count
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "products/all-#{count}-#{max_updated_at}"
+  end
 end
