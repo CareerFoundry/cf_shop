20b4c06d5486db0183d2b6ee05375404fe2111d1
sam <sam.bogu@gmail.com>
2016-02-03 17:31:19 +0100

    added code from exercise to payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,15 +2,21 @@ class PaymentsController < ApplicationController
 
 def create
 	token = params[:stripeToken]
-	product = Product.find(params[:product_id])
+	@product = Product.find(params[:product_id])
+	@user = current_user
 	# Create the charge on Stripe's servers - this will charge the user's card
 	begin
 		charge = Stripe::Charge.create(
-			:amount => 2000, # amount in cents, again
+			:amount => (@product.price*100).to_i, # amount in cents, again
 			:currency => "usd",
 			:source => token,
 			:description => params[:stripeEmail]
 		)
+
+		if charge.paid
+			Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
+		end
+
 		flash[:success] = "Payment processed successfully"
 	rescue Stripe::CardError => e
 		# The card has been declined
@@ -18,7 +24,7 @@ def create
 		err  = body[:error]
 		flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
 	end
-	redirect_to product_path(product)
+	redirect_to product_path(@product)
 end
 
 end
