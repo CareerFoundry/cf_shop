9cea48ed3923d07bfacc06921fb4362faee2b199
sam <sam.bogu@gmail.com>
2016-02-04 09:15:41 +0100

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ActiveRecord::Base
 	has_many :orders
 	has_many :comments
 
-	validates :name, presence: true
+	validates :name, :price, :image_url, :color presence: true
+	validates :price, numericality: true
 
 	def average_rating
 		comments.average(:rating).to_f
