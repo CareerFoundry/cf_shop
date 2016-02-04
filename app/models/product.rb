c4555937271b7c662768a4c9d799b46745b77d5e
sam <sam.bogu@gmail.com>
2016-02-04 09:42:58 +0100

    fixed validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 	has_many :orders
 	has_many :comments
 
-	validates :name, :price, :image_url, :color presence: true
+	validates :name, :price, :image_url, :color, presence: true
 	validates :price, numericality: true
 
 	def average_rating
