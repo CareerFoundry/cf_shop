825d72ffb115ffb0845178652f0b779acea8a103
jimbroski <sam.bogu@gmail.com>
2016-07-13 17:07:54 +0200

    updated devise correctly

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, :only => [:create]
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # protected
+  #
+  #   def configure_permitted_parameters
+  #     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+  #   end
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+  end
 end
