2e19b0f585bbb9cb7d09d69841fe0460ad5c1575
jimbroski <sam.bogu@gmail.com>
2016-07-20 15:40:20 +0200

    DRYer Raty code

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,15 @@
-$(document).on('turbolinks:load', function() {
+var refreshRating = function() {
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets',
 		readOnly: true,
-	  score: function() {
-	    return $(this).attr('data-score');
-	  }
+		score: function() {
+			return $(this).attr('data-score');
+		}
 	});
+};
+
+$(document).on('turbolinks:load', function() {
+	refreshRating();
 
 	$('.img-zoom').elevateZoom();
 });
